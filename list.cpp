1e0c72a61212c4ab102cf93584b7c08de21ac134
Artem <mora4evskiy@yandex.ru>
2024-02-28 22:15:07 +0300

    I finish my List, I add sort, push, removeList

--- list.cpp
+++ list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 //using namespace std;
 
@@ -50,7 +51,62 @@ void append(List* list1, List* list2)
 
 void removeList(List* list)
 {
-    
+    List* current = list;
+    List* nxt;
+
+    while (current != NULL)
+    {
+        nxt = current->next;
+        delete current;
+        current = nxt;
+    }
+}
+
+void push(List* list, int num)
+{
+    List* newNode = new List;
+    newNode->data = num;
+
+    List* tmp = list;
+    while (list->next != NULL)
+    {
+        if (list->next->data >= num)
+        {
+            tmp = list->next;
+            list->next = newNode;
+            newNode->next = tmp;
+            break;
+        }
+
+        tmp = list->next;
+        list = list->next;
+    }
+}
+
+void sort(List* list)
+{
+    int tmp;
+    List* current = list;
+    List* next;
+
+    while (current != NULL) 
+    {
+        next = current->next;
+        
+        while (next != NULL) 
+        {
+            if (current->data > next->data) 
+            {
+                tmp = current->data;
+                current->data = next->data;
+                next->data = tmp;
+            }
+            
+            next = next->next;
+        }
+
+        current = current->next;
+    }
 }
 
 int sumCount(List* list)
@@ -103,39 +159,46 @@ int main()
     List list1;
     List list2;
     List list3;
-    list1.data = 1;
-    list2.data = 2;
-    list3.data = 3;
-
+    List list4;
+    List list6;
+    list1.data = 4;
+    list2.data = 5;
+    list3.data = 6;
+    list4.data = 1;
+    list6.data = 2;
     list1.next = &list2;
     list2.next = &list3;
-    list3.next = NULL;
+    list3.next = &list4;
+    list4.next = &list6;
 
-    std::cout << list1.data << std::endl;
-    std::cout << list1.next->data << std::endl;
-    std::cout << list1.next->next->data << std::endl;
+    list6.next = NULL;
 
-    List* list4 = add_front(10, &list1);
+    //List* list4 = add_front(10, &list1);
     
-    int size = 5;
+    int size = 15;
     int arr[size];
 
     for (int i = 0; i < size; ++i)
     {
         arr[i] = i;
     } 
-    
+
     List* list5 = convertFromArrayToList(arr, size);
     int sum = sumCount(list5);
 
     //cout << sum << endl;
 
-    int elem = nth(list5, 1);
+    //int elem = nth(list5, 1);
 
+    //push(&list1, 4);
     //std::cout << elem << std::endl;
     //removeLastElement(&list1);
-    append(&list1, list5);
+    //append(&list1, list5);
+    sort(&list1);
     Print(&list1);
+    //Print(list5);
+    //removeList(list5);
+    //Print(list5);
 
     return 0;
 }
