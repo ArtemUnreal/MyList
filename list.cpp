99a5196eb8e96e464695997252b44357246b0503
Artem <mora4evskiy@yandex.ru>
2024-03-13 14:55:04 +0300

    add mergeSort, fix quickSort, add reverseList

--- list.cpp
+++ list.cpp
@@ -20,12 +20,25 @@ void Print(List* list)
     std::cout << std::endl;
 }
 
+void removeList(List* list)
+{
+    List* current = list;
+    List* nxt;
+
+    while (current != NULL)
+    {
+        nxt = current->next;
+        delete current;
+        current = nxt;
+    }
+}
+
 List* add_front(int element, List* list)
 {
     List* res = new List;
     (*res).data = element;
-
-    res->next = list;    
+  
+    res->next = list;
     return res;    
 }
 
@@ -59,19 +72,6 @@ void removeLastElement(List* list)
     delete list->next;
 }
 
-void removeList(List* list)
-{
-    List* current = list;
-    List* nxt;
-
-    while (current != NULL)
-    {
-        nxt = current->next;
-        delete current;
-        current = nxt;
-    }
-}
-
 List* append(List* list1, List* list2)
 {
     if (list1 == NULL)
@@ -95,6 +95,7 @@ List* push(List* list, int num)
 {
     List* newNode = new List;
     newNode->data = num;
+    newNode->next = NULL;
 
     if (list == NULL || list->data >= num)
     {
@@ -128,6 +129,22 @@ List* insertionSort(List* list)
     return sorted;
 }
 
+List* reverseList(List* list)
+{
+    List* prev = NULL;
+    List* next = NULL;
+
+    while (list != NULL)
+    {
+        next = list->next;
+        list->next = prev;
+        prev = list;
+        list = next;
+    }
+
+    return prev;
+}
+
 List* quickSort(List* list)
 {
     if (list == NULL) 
@@ -147,15 +164,16 @@ List* quickSort(List* list)
     {
         if (first_elem <= list->data)
         {
-            list1 = push(list1, list->data);
+            list1 = add_front(list->data, list1);
         }
         else
         {
-            list2 = push(list2, list->data);
+            list2 = add_front(list->data, list2);
         }
 
         list = list->next;
     }
+
     List* delList1 = list1;
     List* delList2 = list2;
 
@@ -165,23 +183,120 @@ List* quickSort(List* list)
     removeList(delList1);
     removeList(delList2);
 
-    list2 = push(list2, first_elem);
+    list2 = add_front(first_elem, list2);
 
-    list1 = append(list2, list1);
+    list2 = reverseList(list2);
+    list1 = reverseList(list1);
 
-    return list1;
+    list2 = append(list2, list1);
+
+    //list1 = reverseList(list1);
+    return reverseList(list2);
 }
 
-void merge(List* list1, List* list2)
+List* merge(List* list1, List* list2)
 {
-    while (list1->next != NULL && list2->next != NULL)
+    List* res = NULL;
+
+    while (list1 != NULL && list2 != NULL)
     {
-        List* res;
         if (list1->data < list2->data)
         {
-            res;
+            res = add_front(list1->data, res);
+            list1 = list1->next;
+        }
+        else
+        {
+            res = add_front(list2->data, res);
+            list2 = list2->next;
         }
     }
+
+    if (list1 != NULL)
+    {
+        while (list1 != NULL)
+        {
+            res = add_front(list1->data, res);
+            list1 = list1->next;
+        }
+    }
+    else if (list2 != NULL)
+    {
+        while (list2 != NULL)
+        {
+            res = add_front(list2->data, res);
+            list2 = list2->next;
+        }
+    }
+
+    res = reverseList(res);
+
+    return res;
+}
+
+int len(List* list)
+{
+    int res = 0;
+
+    while (list != NULL)
+    {
+        list = list->next;
+        ++res;
+    }
+
+    return res;
+}
+
+List* mergeSort(List* list)
+{
+    if (list == NULL) 
+    {
+        return list;
+    }
+
+    if (list->next == NULL)
+    {
+        list = add_front(list->data, NULL);
+        return list;
+    }
+
+    int sz = len(list);
+    int half = sz / 2;
+    int count = 0;
+
+    List* list1 = NULL;
+    List* list2 = NULL;
+
+    while (count < half)
+    {
+        list1 = add_front(list->data, list1);
+        list = list->next;
+        ++count;
+    }
+
+    while (count < sz)
+    {
+        list2 = add_front(list->data, list2);
+        list = list->next;
+        ++count;
+    }
+    List* del1 = list1;
+    List* del2 = list2;
+    list1 = mergeSort(list1);
+    list2 = mergeSort(list2);
+
+    removeList(del1);
+    removeList(del2);
+
+    del1 = list1;
+    del2 = list2;
+
+    List* res = merge(list1, list2);
+
+    removeList(del1);
+    removeList(del2);
+
+    return res;
 }
 
 void bubbleSort(List* list)
@@ -238,27 +353,42 @@ List* convertFromArrayToList(int* arr, int size)
 
 int main() 
 {
-    int size = 5;
-    int arr[] = {3, 1, 2, 5, 4};
+    int size = 8;
+    int arr[] = {3, 1, 2, 5, 4, 8, 7, 6};
     List* list = convertFromArrayToList(arr, size);
 
-    Print(list);
+    int size_ = 3;
+    int arr_[] = {8, 7, 6};
+    List* list2 = convertFromArrayToList(arr_, size_);
 
-    List* fun = NULL;
+    //Print(list);
+
+    //list = reverseList(list);
+
+    Print(list);
+    //Print(list2);
+
+    //List* fun = NULL;
 
     //fun = append(fun, list);
 
     List* del = list;
-
     list = quickSort(list);
+    //list = quickSort(list);
     removeList(del);
     //list = push(list, 0);
 
-    std::cout << sizeof(*list) << std::endl;
+    removeList(list2);
+
+    //list = removeFirstElement(list);
+
+    //std::cout << sizeof(*list) << std::endl;
     
-    Print(list);
+    Print(reverseList(list));
 
     removeList(list);
+
+    //removeList(list);
 
     return 0;
 }
